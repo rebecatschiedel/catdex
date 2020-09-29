546dbe67c12d7d763af12c0fa26ea67cfdad325b
apple <apple@apples-imac.mynet>
2020-09-29 12:06:31 +0300

    Updates

---
+++ precache-manifest.9da2d0b67903c830320fd474c436ccd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8520765da815af3c032ce254525a2082",
+    "url": "/catdex/index.html"
+  },
+  {
+    "revision": "80f5bcabdf2b09a87fdd",
+    "url": "/catdex/static/css/main.3991fc8c.chunk.css"
+  },
+  {
+    "revision": "082f8914de693280636e",
+    "url": "/catdex/static/js/2.9cbb02df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/catdex/static/js/2.9cbb02df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f5bcabdf2b09a87fdd",
+    "url": "/catdex/static/js/main.b3daa720.chunk.js"
+  },
+  {
+    "revision": "b37423bfeb760ee00552",
+    "url": "/catdex/static/js/runtime-main.418ecf81.js"
+  }
+]);
